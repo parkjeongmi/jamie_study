fa78d7f69597adb8008894526c520f72a068e5a8
parkjeongmi <jeongmi7593@gmail.com>
2021-02-24 21:30:04 +0900

    2469

--- basic/ing2469.py
+++ basic/ing2469.py
@@ -1,7 +1,13 @@
+# 사다리타기
 import sys
-#k = 참가한 사람의 수
-#n = 가로 막대가 놓일 전체 가로줄의 수
-#result = 결정된 참가자들의 최종 순서 길이 (대문자 문자열)
-k = sys.stdin.readline()
-n = sys.stdin.readline()
-result = sys.stdin.readlin()
+
+#가로 막대가 없는 경우 : *
+#가로 막대가 있는 경우 : -
+#감추어진 특정 가로 줄 : 길이 k-1인 ?
+k = sys.stdin.readline() #k = 참가한 사람의 수 
+n = sys.stdin.readline() #n = 가로 줄의 수 
+result = list(map(str, input().split())) #result = 참가자들의 최중 순서. 길이 k인 대문자 문자열
+for i in range(n) :
+        road = [*map(str, input().split())]
+        print("\n")
+#감추어진 가로 줄 구성
