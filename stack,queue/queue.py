9084374b702c6281c3128f58f8e0dbfb634fb5b3
parkjeongmi <jeongmi7593@gmail.com>
2021-03-02 00:17:06 +0900

    queue01

--- stack,queue/queue.py
+++ stack,queue/queue.py
@@ -22,3 +22,17 @@ def solution(bridge_length, weight, truck_weights):
         if 
             
     return count
+
+
+   
+def solution_answer(bridge_length, weight, truck_weights) :
+    q = [0]*bridge_length
+    sec = 0
+    while q :
+        sec+=1
+        q.pop(0)
+        if truck_weights :
+            if sum(q) + truck_weights[0] <= weight :
+                q.append(truck_weights.pop(0))
+            else : q.append(0)
+    return sec
