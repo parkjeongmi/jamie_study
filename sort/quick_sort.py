b652457bb0cebea6b63f5ddecd4f9a0b15a8ec2b
parkjeongmi <jeongmi7593@gmail.com>
2021-03-04 22:22:24 +0900

    quick_sort

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -1,3 +1,27 @@
 #퀵정렬 : 기준 데이터 설정 -> 그 기준보다 큰 데이터와 작은 데이터의 위치 바꿈
+#시간복잡도 : O(NlogN) 
 ##basic : 첫 번째 데이터를 기준 데이터(pivot)으로 설정
 
+array = [5,7,9,0,3,1,6,2,4,8]
+
+def quick_sort(array, start, end) :
+    if start >= end :
+        return
+    pivot = start
+    left = start+1
+    right = end
+    while(left<=right) :
+        while(left<=end and array[left] <= array[pivot]) :
+            left+=1
+
+        while(right>start and array[right] >= array[pivot]) :
+            right -=1
+        if (left>right) :
+            array[right], array[pivot] = array[pivot], array[right]
+
+        else :
+            array[left], array[right] = array[right], array[left]
+    quick_sort(array, start, right-1)
+    quick_sort(array, right+1, end)
+quick_sort(array, 0, len(array)-1)
+print(array)
