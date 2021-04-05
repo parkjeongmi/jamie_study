aa1550a8cce09496ee26edc2c544e55b5781f586
parkjeongmi <jeongmi7593@gmail.com>
2021-04-05 19:47:49 +0900

    binary_search01.py

---
+++ binary_search/binary_search01.py
@@ -0,0 +1,26 @@
+#이진탐색 (재귀함수 이용)
+
+def binary_search(array, target, start, end) :
+    mid = (start+end) // 2
+    if start > end : 
+        return 0
+
+    if array[mid] == target : 
+        return mid
+    elif array[mid] < target : 
+        return binary_search(array, target, mid+1, end)
+    else : 
+        return binary_search(array, target, start, mid-1)
+    
+
+
+
+n, target = list(map(int, input().split()))
+array = list(map(int, input().split()))
+result = binary_search(array, target, 0, n-1)
+if result == None :
+    print("없음")
+else : print(result+1)
+
+#10 7
+# 1 3 5 7 9 11 13 15 17 19
--- greedy/greedy03.py
+++ greedy/greedy03.py
@@ -1,8 +1,31 @@
 #프로그래머스
 #그리디
 #큰 수 만들기
-def solution(number) :
-    numbers= int(number)
+def solution(number, k) :
     number_ing = []
-    for i in len(numbers) :
+    for i in number :
         number_ing.append(i)
+    number_ing.sort(reverse=True)
+    while (len(number_ing) != k ) :
+        del number_ing[len(number_ing)]
+    for i in number_ing :
+        return ''.join(i)
+
+number = "19245" 
+k = 2
+print(solution(number,k))
+#문자열 형식으로 숫자 number, 제거할 수의 개수 k -> number에서 k 개의 수 제거
+
+def answer(number, k) : 
+    number_ing = []
+    for i, num in enumerate(number) :
+        while len(number_ing) > 0 and number_ing[-1] < num and k > 0 :
+            number_ing.pop()
+            k-=1
+        if k == 0 :
+            number_ing += list(number[i:])
+            break
+        number_ing.append(num)
+    number_ing = number_ing[:-k] if k > 0 else collected
+    answer = ''.join(collected)
+    return answer
