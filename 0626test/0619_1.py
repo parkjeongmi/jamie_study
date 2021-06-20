7fc12fe3269c8ac24c69477f3d69df59101c9209
parkjeongmi <jeongmi7593@gmail.com>
2021-06-20 23:33:04 +0900

    0619

---
+++ 0626test/0619_1.py
@@ -0,0 +1,25 @@
+#프로그래머스
+#스택/큐
+#기능개발
+from collections import deque
+import math
+progresses = [93,30,55]
+speeds = [1,30,5]
+
+from collections import deque
+import math
+def solution(progresses, speeds):
+    todo = deque()
+    for i in range(len(progresses)) :
+        todo.append(math.ceil((100-progresses[i])/speeds[i]))
+    answer = []
+    while todo :
+        count = 1
+        now = todo.popleft()
+        while len(todo)>0 :
+            if todo[0] <= now :
+                todo.popleft()
+                count += 1
+            else : break
+        answer.append(count)
+    return answer
