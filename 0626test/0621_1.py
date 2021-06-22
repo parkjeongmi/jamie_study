2d2274a477848d037040464447b6c3caff06ece2
parkjeongmi <jeongmi7593@gmail.com>
2021-06-23 01:05:14 +0900

    0621

--- 0626test/0621_1.py
+++ 0626test/0621_1.py
@@ -2,26 +2,15 @@
 #스택/큐
 
 from collections import deque
-priorities = [1, 1, 9, 1, 1, 1]
-location = 0
-answer = []
-while priorities :
-    #2. priorities를 선입선출
-    now = priorities.pop(0)
-    if location == 0 :
-        location = len(priorities)
-    else :
-        location -= 1
-    #3. pop보다 큰 값이 있으면 append
-    if max(priorities) > now :
-        priorities.append(now)   
-    #3-2. 없으면 제거
-    else :
-        answer.append(now)
-        #4. location == index 값이 제거되는 순서 출력
-        if now == priorities[location] :
-            print(len(answer))
-            break
-
-
-
+def solution(priorities, location):
+    d = deque([(v, i) for i,v in enumerate(priorities)])
+    answer = 0
+    while len(d) :
+        now = d.popleft()
+        if d and max(d)[0] > now[0] :
+            d.append(now)
+        else :
+            answer +=1
+            if now[1] == location :
+                break
+    return answer
---
+++ 0626test/0622_1.py
@@ -0,0 +1,16 @@
+#프로그래머스
+#다리를 지나는 트럭
+
+bridge_length = 2
+weight = 10
+truck_weights = [7,4,5,6]
+index = [0] * for _ in range(len(truck_weights))
+print(index)
+
+going  = []
+while len(truck_weights) :
+    now = truck_weight.popleft()
+    for i in range(bridge_length) :
+
+
+
