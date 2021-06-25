c1a7c5dc6c101f72242d70636e00d02dc2e204f2
jeongmi <47415255+parkjeongmi@users.noreply.github.com>
2021-06-25 23:18:42 +0900

    0625

--- 0626test/0624_1.py
+++ 0626test/0624_1.py
@@ -3,35 +3,17 @@
 participant = ["mislav", "stanko", "mislav", "ana"]
 completion = ["stanko", "ana", "mislav"]
 
-dic = {}
-for p in participant :
-    if p not in dic.keys() :
-        dic[p] = 1
-    else :
-        dic[p] = 2
-
-for com in completion :
-    if com in dic.keys() :
-        dic[com] -= 1
-answer = 0
-for key, value in dic.items() :
-    if value != 0 :
-        answer = key
-print("".join(answer))
-
 def solution(participant, completion):
-
     dic = {}
     for p in participant :
         if p not in dic.keys() :
             dic[p] = 1
         else :
-            dic[p] = 2
+            dic[p] += 1
 
-    for com in completion :
-        if com in dic.keys() :
-            dic[com] -= 1
-    answer = 0
+    for c in completion :
+        if c in dic.keys() :
+            dic[c] -= 1
     for key, value in dic.items() :
-        if value != 0 :
+        if value > 0 :
             return(key)
---
+++ 0626test/0625_1.py
@@ -0,0 +1,23 @@
+#백준
+#최소 스패닝 트리
+
+#모든 정점 연결 + 가중치의 합 최소
+
+#Kruskal
+#1. 간선 정렬
+#2. 간선이 잇는 두 정점의 root를 찾음
+#3. 다르다면 하나의 root를 바꾸어 연결
+
+def find_parent(parent, x) :
+    if parent[x] != x :
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+def make_union(parent, a, b) :
+    a = find_parent(parent,a)
+    b = find_parent(parent, b)
+    if a<b :
+        parent[b] = a
+    else :
+        parent[a] = b
+
+V, E = map(int)
